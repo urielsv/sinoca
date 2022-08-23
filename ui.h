9505e64ea9c60fdebfc6607ac32767f5aebfe399
feliventu <feliventurino@gmail.com>
2022-08-23 20:21:35 -0300

    Added ui headfile

---
+++ ui.h
@@ -0,0 +1,9 @@
+#include "game.h"
+//funciones que imprimen las filas y columnas de #
+void rowBoard(void);
+void colBoard(void);
+
+
+//funcion que lee el numero que sale e imprime lo que salio en grande
+void slotToBoard(deck num);
+
